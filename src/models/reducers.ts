e755fad6cf7aae8b7e4266bd8fdf84211bdb6049
Cathy Retina 13 <a304126@gmail.com>
2020-12-04 14:33:23 +0800

    fix: remove uesless reducer

--- src/models/reducers.ts
+++ src/models/reducers.ts
@@ -1,20 +1,14 @@
 import { combineReducers } from 'redux';
 
 import * as routing from './routing';
-import * as members from './member';
-import * as blogs from './blog';
 
 // For Global State interface
 export interface State {
-	blogs: blogs.State;
-	members: members.State;
 	routing: routing.State;
 }
 
 const reducers = combineReducers<State>({
 	...routing.reducer,
-	...members.reducer,
-	...blogs.reducer,
 });
 
 export default reducers;
